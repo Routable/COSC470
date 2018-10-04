cb8db0f867856f207cead64fdd955b4e7d538b27
Steven B <Stevenabucholtz@gmail.com>
2018-10-04 09:15:12 -0700

    Adding the second lab

---
+++ LAB2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class StringOperators
+    {
+        public string concatonate(String X, String Y)
+        {
+            string merged = X + Y;
+            return merged;
+        }
+
+        public bool comparison(String X, String Y)
+        {
+
+            if (X == Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string copystring(string x)
+        {
+            string y = x;
+            return y;
+        }
+
+        public bool empty(string x)
+        {
+            if(String.IsNullOrEmpty(x))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ LAB2/ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            StringOperators s = new StringOperators();
+            string x = s.concatonate("Hello", "World");
+            string y = "Hello World";
+            Assert.AreEqual(x, y);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            StringOperators s = new StringOperators();
+            bool x = s.comparison("Hello", "Helldo");
+            bool y = true;
+            Assert.AreEqual(x, y);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            StringOperators s = new StringOperators();
+            string x = s.copystring("Hello");
+            string y = "Hello";
+            Assert.AreEqual(x, y);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            StringOperators s = new StringOperators();
+            bool x = s.empty("");
+            bool y = true;
+            Assert.AreEqual(x, y);
+        }
+    }
+}
