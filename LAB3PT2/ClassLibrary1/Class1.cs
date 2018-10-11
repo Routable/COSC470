94c06ee1f64fcb0f2a6a2de1c102e6cb78627874
Steven B <Stevenabucholtz@gmail.com>
2018-10-11 10:18:20 -0700

    Part 2 of Lab 3

---
+++ LAB3PT2/ClassLibrary1/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab3P2;
+using NUnit.Framework;
+
+namespace Lab3P2
+{
+    [TestFixture]
+    public class Testing
+    {
+        [Test]
+        public void testLogin()
+        {
+            Program source = new Program();
+            string result = source.Login("Admin", "Admin");
+            Assert.AreEqual(result, "Welcome User/Admin");
+        }
+
+        [Test]
+        public void testListFirst()
+        {
+            Program source = new Program();
+            List<StudentDetails> result = source.AllUsers();
+            StudentDetails result2 = result.First();
+            Assert.AreEqual("steven", result2.getName());
+        }
+
+        [Test]
+        public void testLoop()
+        {
+            Program source = new Program();
+            List<StudentDetails> result = source.AllUsers();
+
+            foreach (var x in result)
+            {
+                Assert.AreEqual(x.getId(), 300168440);
+            }
+        }
+
+        [Test]
+        public void testListNull()
+        {
+            Program source = new Program();
+            List<StudentDetails> result = source.AllUsers();
+
+            foreach (var x in result)
+            {
+                Assert.IsNotNull(x.getName());
+            }
+        }
+    
+
+    }
+}
---
+++ LAB3PT2/Lab3P2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3P2
+{
+    public class StudentDetails
+    {
+        private string name;
+        private int id; 
+
+        public StudentDetails(string studentname, int studentid)
+        {
+            name = studentname;
+            id = studentid;
+        }
+
+        public int getId()
+        {
+
+            return id;
+        }
+
+        public void setId(int newid)
+        {
+            id = newid;
+        }
+
+        public string getName()
+        {
+
+            return name;
+        }
+
+        public void setName(string newname)
+        {
+            name = newname;
+        }
+    }
+    public class Program
+    {
+        public string Login(string username, string password)
+        {
+            if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                return "Username or Password could not be empty.";
+            } else if(username == "Admin" && password == "Admin")
+            {
+                return "Welcome User/Admin";
+            } else
+            {
+                return "Incorrect login...";
+            }
+        }
+
+        public List<StudentDetails> AllUsers()
+        {
+            List <StudentDetails> li = new List<StudentDetails>();
+            li.Add(new StudentDetails("steven", 300168440));
+            li.Add(new StudentDetails("john", 300168440));
+            li.Add(new StudentDetails("test", 300168440));
+            return li;
+        }
+        
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
