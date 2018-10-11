bc31db5d62c3d1bee80db5f219043dce6da97221
Steven B <Stevenabucholtz@gmail.com>
2018-10-11 08:49:16 -0700

    Adding Lab 3

---
+++ Lab3/ClassLibrary1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab3;
+using NUnit.Framework;
+
+namespace Lab3
+{ 
+
+    [TestFixture]
+    public class AccountTest
+    {
+        [Test]
+        public void TransferFunds()
+        {
+            Account source = new Account();
+            source.Deposit(200m);
+
+            Account destination = new Account();
+            destination.Deposit(150m);
+
+            source.TransferFunds(destination, 100m);
+
+            Assert.AreEqual(250m, destination.Balance);
+            Assert.AreEqual(100m, source.Balance);
+        }
+    }
+    public class Class1
+    {
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+        public class Account
+        {
+            private decimal balance;
+
+            public void Deposit(decimal amount)
+            {
+                balance += amount;
+            }
+
+            public void Withdraw(decimal amount)
+            {
+                balance -= amount;
+            }
+
+            public void TransferFunds(Account destination, decimal amount)
+            {
+            }
+
+            public decimal Balance
+            {
+                get { return balance; }
+            }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
