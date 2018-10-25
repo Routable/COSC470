be7991f4b386e3f3a215e5eedfbd4ef32af429af
Steven B <Stevenabucholtz@gmail.com>
2018-10-25 10:22:09 -0700

    Lab 5 - Creating Unit Tests for MVC

---
+++ LAB4/Lab4/TestProject/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab4.Controllers;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var controller = new HomeController();
+        var result = controller.Details(2) as ViewResult;
+        //Asert.AreEqual(result.Model, result.Model);
+        Assert.IsNotNull(result);
+        }
+}
