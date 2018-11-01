4556784ae1818d6184ffdf2f829cb65ec7878e37
Steven B <Stevenabucholtz@gmail.com>
2018-11-01 10:11:51 -0700

    Lab 7 - Selenium

---
+++ LAB7/LAB7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace LAB7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://google.ca";
+        }
+    }
+}
