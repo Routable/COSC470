7419987da6e5488765673b88eb67906990109c89
Steven B <Stevenabucholtz@gmail.com>
2018-11-08 09:55:29 -0800

    Selenium Lab - Part 2

---
+++ LAB8/Selenium2/Selenium2/Class1.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+
+
+
+
+namespace Selenium2
+{
+    public class Class1
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "http://blog.testproject.io/";
+        }
+
+        [Test]
+        public void test()
+        {
+            IWebElement element = driver.FindElement(By.LinkText("Contact Us"));
+            element.Click();
+
+            IWebElement element2 = driver.FindElement(By.Name("your-name"));
+            element2.SendKeys("Steven");
+
+            IWebElement element3 = driver.FindElement(By.Name("your-email"));
+            element3.SendKeys("Steven@gmail.com");
+
+            IWebElement element4 = driver.FindElement(By.Name("your-subject"));
+            element4.SendKeys("hello");
+
+            driver.FindElement(By.XPath("//input[@value='Send']")).Submit();
+        }
+
+        [TearDown]
+        public void exit()
+        {
+            driver.Close();
+        }
+
+    }
+}
+
+
+
